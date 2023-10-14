0413f5f142171da24134b6d7936412216d2c75c7
Vivek Vardhan Reddy <vivekvardhanboreddy@gmail.com>
2023-10-14 13:24:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>B Vivek Vardhan Reddy</h1>
+        <p>Web Developer</p>
       </header>
+      <main className="App-main">
+        <section className="About" id="about">
+          <h2>About Me</h2>
+          <p>Hardworking College Student seeking employment. Bringing forth a motivated attitude and a variety of powerful skills. Adept in various social media platforms and office technology programs. Committed to utilizing my skills to further the mission of a company</p>
+        </section>
+        <section className="Projects" id="projects">
+          <h2>Projects</h2>
+          <div className="Project">
+            <h3>Project 1</h3>
+            <p>Arduino Based College Group Project</p>
+          </div>
+        </section>
+        <section className="Contact" id="contact">
+          <h2>Contact</h2>
+          <p>Email: vivekvardhanboreddy@gmail.com</p>
+          <p>LinkedIn: <a href="https://www.linkedin.com/in/vivek-vardhan-reddy-3a843124b/">Your LinkedIn Profile</a></p>
+          <p>Github: <a href="https://github.com/Vivekvardhanreddy1707">Your GitHub Profile</a></p>
+        </section>
+      </main>
     </div>
   );
 }
